698fb961e5fcd3c79525718cdb4cdcc84ab05dd6
nicosolano <nicoesolano@gmail.com>
2024-05-29 17:24:04 -0300

    cards + nosotros

--- js/cards.js
+++ js/cards.js
@@ -2,9 +2,24 @@ fetch('js/movies.json')
 .then(response => response.json())
 .then(data => {
     const cards = data.map(movie => 
-        `<div class="card">
-            <img src="${movie.Poster}">
+        `<div class="card" id="${movie.id}">
+            <img src="${movie.Poster}" alt='Poster promocional de la película "${movie.Title}"'>
         </div>`
     ).join('');
-    document.querySelector('.card-container').innerHTML = cards;
+    document.querySelector('.container').innerHTML = cards;
+
+    const info = data.map(movie =>
+        `<div class="info-container">
+            <img src="${movie.Poster}" alt='Poster promocional de la película "${movie.Title}"'>
+            <div class="info">
+                <h3>${movie.Title} (${movie.Year})</h3><span>${movie.Rated}</span>
+                <p>Duración: ${movie.Runtime}</p>
+                <p>Género: ${movie.Genre}</p>
+                <p>Sinopsis: ${movie.Plot}</p>
+                <p>Protagonizada por: ${movie.Actors}</p>
+                <p>Dirigida por: ${movie.Director}</p>
+            </div>
+        </div>`
+    ).join('');
+
 })
